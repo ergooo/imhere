65f8eb137a8bf540d6c09e4e5b5dbb8a93b09053
masato <goodmoon.badmoon@gmail.com>
2013-03-06 13:12:40 +0900

    add startup receiver.

---
+++ src/jp/ergo/android/imhere/StartupReceiver.java
@@ -0,0 +1,16 @@
+package jp.ergo.android.imhere;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class StartupReceiver extends BroadcastReceiver{
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+
+		Intent serviceIntent = new Intent(context, ImhereService.class);
+		context.startService(serviceIntent);
+	}
+
+}
